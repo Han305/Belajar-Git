38e3c3bff44394daee572746347dabc84b5956b6
Han305 <rayhan.hidayat18@gmail.com>
2023-03-28 12:55:44 +0700

    starter project

---
+++ src/main/java/com/rayhan/training/git/GuessBookApplication.java
@@ -0,0 +1,13 @@
+package com.rayhan.training.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rayhan/training/git/GuessBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayhan.training.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
